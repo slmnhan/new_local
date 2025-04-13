1232c8c6774fd5c547036aae6b0f2dc23f322327
Student slmnhan <salkan9601@gmail.com>
2025-04-13 19:17:48 +0500

    my first Attempt of understanding of recursion

--- python.py
+++ python.py
@@ -1,1 +1,13 @@
-print("this is my new repo")
+def factorial(n):
+    space = ' ' * (4 * n)
+    print(space, 'factorial', n)
+    if n == 0:
+        print(space, 'returning 1')
+        return 1
+    else:
+        recurse = factorial(n-1)
+        result = n * recurse
+        print(space, 'returning', result)
+        return result
+    
+print(factorial(5))
--- readme.md
+++
@@ -1,2 +0,0 @@
-THIS IS MY README FILE
-what if i say this is my new line @@
