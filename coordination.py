2dda431a25c59122d75902e29151db46f3a7471b
Student slmnhan <salkan9601@gmail.com>
2025-04-16 11:52:25 +0500

    added with the help of virtual assistant

--- coordination.py
+++ coordination.py
@@ -1,10 +1,9 @@
 import math
-def distance_between_two_points(x1, y1, x2, y2):
 
-# We know that the distance between two ponts is calculated by using the formula square root of  (x1-x1)^2 + (y2-y2)^2
-#  so know iam going to return the mathemaitcal equasion of this by putiing values in this formula
-    return math.sqrt( (x1 - x2)**2 + (y1 - y2)**2 )
-# i hope this function will calculate the distance between two points
-#  Now let's run this function and see if it works?
-#  there is a thing in python that before running of the code we run predictoin in our brain and that is what give the programmer confidence to run the code
+def distance_between_two_points(x1, y1, x2, y2):
+    # The distance between two points is calculated using the formula:
+    # √[(x2 - x1)^2 + (y2 - y1)^2]
+    return math.sqrt((x2 - x1)**2 + (y2 - y1)**2)
+
+# Let's test the function with an example
 print(distance_between_two_points(5, 3, 8, 7))
